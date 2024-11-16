0b78ca6487943c7a3fd12500d62a6b2e6f244880
dawidi-m <dawidi1818@gmail.com>
2024-11-16 22:39:42 +0200

    initial info updated

---
+++ src/resumeData.js
@@ -0,0 +1,95 @@
+let resumeData = {
+  "imagebaseurl":"https://github.com/dawidi-m/",
+  "name": "David Manzi ",
+  "role": "software engineer",
+  "linkedinId":"david-manzi-8b6a071ba",
+  "roleDescription": " With skills in both backend and frontend development, I’m passionate about coding, requirement analysis, software design, and data visualization to create efficient solutions.",
+  "socialLinks":[
+      {
+        "name":"linkedin",
+        "url":"https://www.linkedin.com/in/david-manzi-8b6a071ba/",
+        "className":"fa fa-linkedin"
+      },
+      {
+        "name":"github",
+        "url":"http://github.com/dawidi-m",
+        "className":"fa fa-github"
+      }
+    ],
+  "aboutme":"Hi! I’m David Manzi, a Master's graduate in Information Technology from Carnegie Mellon University Africa. I also hold a degree in Software Engineering from the Adventist University of Central Africa, graduated in 2023. Currently, I work as an Application Specialist at King Faisal Hospital, Rwanda. My goal is to grow into a skilled developer and product owner, creating impactful solutions that benefit diverse communities. I’m passionate about exploring history, politics, and sports, and I enjoy meaningful conversations. Though often considered a quiet black guy, my smile says it all. 😊",
+  "address":"Kigali, Rwanda",
+  "website":"dawidi1818@gmail.com",
+  "education":[
+    {
+      "UniversityName":"CMU Africa",
+      "specialization":"Masters in Information Technology",
+      "MonthOfPassing":"May",
+      "YearOfPassing":"2023",
+      // "Achievements":"Some Achievements"
+    },
+    {
+      "UniversityName":"Adventist University of Central Africa (Auca)",
+      "specialization":"Information Technology-Software Engineering",
+      "MonthOfPassing":"Nov",
+      "YearOfPassing":"2021",
+      // "Achievements":"Some Achievements"
+    }
+  ],
+  "work":[
+    {
+      "CompanyName":"King Faisal Hospital Rwanda",
+      "specialization":"Application Specialist",
+      "MonthOfLeaving":"Nov",
+      "YearOfLeaving":"2023",
+      "Achievements":"Ensuring Reliability and Performance of HMIS, Feedback gathering,Web development, Training"
+    },
+    {
+      "CompanyName":"Minecofin",
+      "specialization":"Software consultant",
+      "MonthOfLeaving":"Sept",
+      "YearOfLeaving":"2022",
+      "Achievements":"NLP Research-Documentation-Frontend "
+    },
+    {
+      "CompanyName":"ComzAfrica",
+      "specialization":"Software Engineer intern",
+      "MonthOfLeaving":"May",
+      "YearOfLeaving":"2022",
+      "Achievements":"Portal Creation - 4 Microservices created"
+    }
+  ],
+  "skillsDescription":"Some of my skills",
+  "skills":[
+    { skillname: 'Java', skillLevel: 80 },
+    { skillname: 'Python', skillLevel: 83 },
+    { skillname: 'JS Frameworks', skillLevel: 70 },
+    { skillname: 'COMMUNICATION', skillLevel: 90 },
+    { skillname: 'ADAPTABILITY', skillLevel: 95 },
+    { skillname: 'TEAM WORK', skillLevel: 90 }
+  ],
+  "portfolio":[
+    {
+      "name":"Emergency Social Network(ESN)",
+      "description":"Web",
+      "imgurl":"images/portfolio/emergency.PNG"
+    },
+    {
+      "name":"Electricity Vending System(EVS)",
+      "description":"USSD",
+      "imgurl":"images/portfolio/evs.PNG"
+
+    },
+    {
+      "name":"Hoodo",
+      "description":"mobileapp",  
+      "imgurl":"images/portfolio/hoodo.PNG"
+    },
+    {
+      "name":"E-Citizen Budget Engagement Project",
+      "description":"Web",
+      "imgurl":"images/portfolio/ecitizen.PNG"
+    }
+  ]
+}
+
+export default resumeData
